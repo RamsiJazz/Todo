19d050ab2075d0ad4e73fe95b22ef3f3248b991f
RamsiJazz <ramsi.jazz@gmail.com>
2022-03-30 19:10:02 +0400

    Updated view with update

--- todo/core/urls.py
+++ todo/core/urls.py
@@ -1,7 +1,9 @@
+from unicodedata import name
 from django.urls import path
 
-from core.views import home
+from core.views import home, update
 
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('update/<int:todo_id>/',update,name='update')
 ]
--- todo/core/views.py
+++ todo/core/views.py
@@ -14,3 +14,13 @@ def home(request):
             form.save()
             return redirect('home')
     return render(request,'home.html',{'forms':form, 'todo':todos})
+
+def update(request,todo_id):
+    todo=Todo.objects.get(id=todo_id)
+    form = TodoForm(instance=todo)
+    if request.method =='POST':
+        form = TodoForm(request.POST,instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'update.html',{'forms':form})
