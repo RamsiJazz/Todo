9e7f61e3d8414362093456938a5d47c049b1cbce
RamsiJazz <ramsi.jazz@gmail.com>
2022-03-30 18:06:48 +0400

    Updated view with POST and db

--- todo/core/urls.py
+++ todo/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from core.views import home
 
 urlpatterns = [
-    path('',home)
+    path('',home,name='home')
 ]
--- todo/core/views.py
+++ todo/core/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from core.forms import TodoForm
+from core.models import Todo
 
 # Create your views here.
 
 def home(request): 
     form = TodoForm()
-    return render(request,'home.html',{'forms':form})
+    todos = Todo.objects.all()
+    if request.method == "POST":
+        form= TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'home.html',{'forms':form, 'todo':todos})
