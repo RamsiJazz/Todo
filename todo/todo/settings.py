3b4c83e35e51ab838d0391b94d9aa8474c0b059e
RamsiJazz <ramsi.jazz@gmail.com>
2022-04-02 12:27:59 +0400

    added Profile

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
+from re import S
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'todo/static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -125,3 +128,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'todo/static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK ='bootstrap4'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
